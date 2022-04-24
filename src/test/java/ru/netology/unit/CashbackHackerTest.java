4d8dc704f8691449cda8a0028a8882b52d96777f
Elena_An <e.anikey@gmail.com>
2022-04-24 17:05:30 +0300

    added testng

---
+++ src/test/java/ru/netology/unit/CashbackHackerTest.java
@@ -0,0 +1,40 @@
+package ru.netology.unit;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class CashbackHackerTest {
+
+    CashbackHacker service = new CashbackHacker();
+
+    @Test
+    public void coastBelowLimit() {
+        int amount = 999;
+
+        int actual = service.remain(amount);
+        int expected = 1;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void bonusEqualLimit() {
+        int amount = 1000;
+
+        int actual = service.remain(amount);
+        int expected = 1000;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void maxBonus() {
+        int amount = 1820;
+
+        int actual = service.remain(amount);
+        int expected = 180;
+
+        assertEquals(actual, expected);
+    }
+}
